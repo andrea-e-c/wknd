48d70c95605f7a15f510df7472375b69fded25da
Andrea Childress <aesselman0830@gmail.com>
2021-09-30 22:25:33 -0400

    Trying to have tray

--- public/electron.js
+++ public/electron.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, Tray, Menu, nativeImage, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 
 function createWindow() {
@@ -26,10 +26,25 @@ function createWindow() {
   }
 }
 
+let tray
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow).then(()=>{
+    const icon = nativeImage.createFromPath('/favicon.ico')
+    tray = new Tray(icon)
+
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
